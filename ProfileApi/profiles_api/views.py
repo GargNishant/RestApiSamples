c0036c3cab894ff29e1ba101811aa6326206c5bc
Nishant Garg <nishantgarg76342@gmail.com>
2020-07-12 13:54:55 +0530

    *(views.py) Post method and use of serializers

--- ProfileApi/profiles_api/views.py
+++ ProfileApi/profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     # noinspection PyMethodMayBeStatic
     def get(self, request, format=None):
@@ -18,4 +23,17 @@ class HelloApiView(APIView):
             'Gives you the most control over your application logic',
             'Is mapped manually to URLs'
         ]
-        return Response({'message': 'Hello!', 'an_apiview': an_api})
+        return Response({'message': 'Hello!', 'an_apiview': an_api}, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        """Create a hello message with our name"""
+
+        # The data is passed as request.data. We then
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
