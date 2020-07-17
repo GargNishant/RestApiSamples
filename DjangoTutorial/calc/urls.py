055229320b9d6a8a973c7f4992de2ee4fd4013d5
Nishant Garg <nishantgarg76342@gmail.com>
2020-07-17 14:20:54 +0530

    *(DjangoTutorial/calc) Method Name changed

--- DjangoTutorial/calc/urls.py
+++ DjangoTutorial/calc/urls.py
@@ -4,6 +4,6 @@ from calc import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('add', views.add, name="Add"),
-    path('add-post', views.addPost, name='Add Post'),
+    path('add-post', views.add_post, name='Add Post'),
     path('add-form', views.addPostForm, name="Add Form")
 ]
--- DjangoTutorial/calc/views.py
+++ DjangoTutorial/calc/views.py
@@ -20,7 +20,7 @@ def add(request):
 
 
 @csrf_exempt
-def addPost(request):
+def add_post(request):
     """Adds 2 numbers, coming as num1 and num2 in POST Request. The API expects application/json
     content type. After Addition, it gives Result in HttpResponse"""
     request_json = json.loads(request.body)
