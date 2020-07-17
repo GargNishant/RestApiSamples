bac7a75490ea0b0ee8d1a18b5720733b83cedba8
Nishant Garg <nishantgarg76342@gmail.com>
2020-07-17 07:44:54 +0530

    +(DjangoTutorial/calc/urls) New Urls +(DjangoTutorial/calc/views) New Post Method

--- DjangoTutorial/calc/urls.py
+++ DjangoTutorial/calc/urls.py
@@ -3,5 +3,6 @@ from calc import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('add/', views.add, name="Add")
+    path('add', views.add, name="Add"),
+    path('add-post', views.addPost, name='Add Post'),
 ]
--- DjangoTutorial/calc/views.py
+++ DjangoTutorial/calc/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+# Create your views here.
+import json
+
 from django.http import HttpResponse
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
@@ -9,7 +12,19 @@ def home(request):
 
 
 def add(request):
+    """Adds 2 numbers, coming as num1 and num2 in simple GET Request. After
+    Addition, it gives Result in HttpResponse"""
     val1 = int(request.GET['num1'])
     val2 = int(request.GET['num2'])
     return HttpResponse(f"Result {val1+val2}")
 
+
+@csrf_exempt
+def addPost(request):
+    """Adds 2 numbers, coming as num1 and num2 in POST Request. The API expects application/json
+    content type. After Addition, it gives Result in HttpResponse"""
+    request_json = json.loads(request.body)
+    val1 = int(request_json["num1"])
+    val2 = int(request_json["num2"])
+
+    return HttpResponse(f'Result is: {val1+val2}')
