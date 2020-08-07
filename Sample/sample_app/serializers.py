9ffe79e5da33dbd115d816c72eb36439aac89697
Nishant Garg <nishantgarg76342@gmail.com>
2020-08-07 17:36:26 +0530

    *(Sample/sample_app/serializers) Completed updated method

--- Sample/sample_app/serializers.py
+++ Sample/sample_app/serializers.py
@@ -9,11 +9,11 @@ class UserProfileSerializer(serializers.Serializer):
     address = serializers.CharField()
     mobile = serializers.IntegerField()
     email = serializers.EmailField()
-    isActive = serializers.BooleanField()
+    isActive = serializers.BooleanField(default=True)
     # DOCS: https://www.django-rest-framework.org/api-guide/fields/
-    # Write only makes the field to be only created or modified, not avail for reading
+    # Write only makes the field to be only created or modified, not avail for reading or GET
     password = serializers.CharField(min_length=10, max_length=200, write_only=True)
-    # Read only make it only for Read. Ignored when trying to modify or create it by args
+    # Read only make it only for Read or GET. Ignored when trying to modify or create it by args or POST,PUT etc
     id = serializers.IntegerField(read_only=True)
 
     def update(self, instance, validated_data):
@@ -26,6 +26,8 @@ class UserProfileSerializer(serializers.Serializer):
         instance.firstName = validated_data.get('firstName', instance.firstName)
         instance.lastName = validated_data.get('lastName', instance.lastName)
         instance.address = validated_data.get('address', instance.address)
+        instance.email = validated_data.get('email', instance.email)
+        instance.mobile = validated_data.get('mobile', instance.mobile)
 
         instance.save()
         return instance
