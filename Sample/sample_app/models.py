3267d62a64cdadc2a2175fb9a7a1973fb4c1cf96
Nishant Garg <nishantgarg76342@gmail.com>
2020-08-07 17:34:17 +0530

    *(Sample/sample_app/models) New Constraints

--- Sample/sample_app/models.py
+++ Sample/sample_app/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 class UserProfile(models.Model):
     firstName = models.CharField(max_length=30, blank=False)
-    lastName = models.CharField(max_length=100, blank=False)
+    lastName = models.CharField(max_length=100, blank=False, null=False)
     address = models.TextField()
-    mobile = models.IntegerField()
-    email = models.CharField(max_length=200, blank=True, default="")
+    mobile = models.IntegerField(unique=True)
+    email = models.CharField(max_length=200, blank=False, unique=True)
     isActive = models.BooleanField(default=True)
-    password = models.CharField(max_length=200, blank=False, default="password")
+    password = models.CharField(max_length=200, blank=False,null=False)
 
     def __str__(self):
         return self.firstName
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
 
 class Sessions(models.Model):
     # Work in Progress
-    userId = models.IntegerField()
+    user = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE)
     session = models.TextField()
     createdAt = models.DateTimeField(auto_now_add=True)
-    deviceDetail = models.TextField()
+    deviceDetail = models.TextField(default="NA", blank=True)
