7d57b118b5a21d833056791cf5aad9c69f456ec9
Nishant Garg <nishantgarg76342@gmail.com>
2020-08-08 14:57:56 +0530

    +(Sample/sample_app/urls) Session Url

--- Sample/sample_app/urls.py
+++ Sample/sample_app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from sample_app import views
 
 
 urlpatterns = [
     path('user-profiles', views.UserProfileView.as_view()),
-    path('user-profiles/<int:pk>', views.UserProfileView.as_view())
+    path('user-profiles/<int:pk>', views.UserProfileView.as_view()),
+    path('sessions', views.SessionView.as_view())
     # path('', include(router.urls))
 ]
