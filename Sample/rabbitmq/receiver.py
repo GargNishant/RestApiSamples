a60a67992a13be512417533d0e66d9abbbbb8886
Nishant Garg <nishantgarg76342@gmail.com>
2020-08-10 22:22:22 +0530

    *(Sample/rabbitmq) Converted to Classes

--- Sample/rabbitmq/receiver.py
+++ Sample/rabbitmq/receiver.py
@@ -1,15 +1,36 @@
 import pika
+import mysql_connect
+import ast
+from datetime import datetime
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+class Receiver(object):
+
+    def __init__(self, queue='hello') -> None:
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self._channel = self._connection.channel()
+        self._queue = queue
+        self._channel.queue_declare(queue=self._queue)
+
+    def start_consuming(self):
+        self._channel.basic_consume(queue=self._queue, on_message_callback=self.callback, auto_ack=True)
+        print("[*] Waiting for Messages. To Exit press CTRL+C")
+        self._channel.start_consuming()
+
+    def callback(self, ch, method, prop, body):
+        print("[x] Received %r" % body)
+        mysql = mysql_connect.MySqlConnect(database='profiles')
+        print(type(str(body)))
+        json = ast.literal_eval(body.decode('utf-8'))
+        device_detail = "device_details"
+        session = str(datetime.now())
+        result = mysql.get_records(query=f"INSERT INTO sample_app_sessions (session, createdAt, deviceDetail,"
+                                f" user_id) VALUES ('{session}', '{session}', "
+                                         f"'{device_detail}', {int(json['user_id'])});")
+        print(result)
+        mysql.close_connection()
 
 
-def callback(ch, method, prop, body):
-    print("[x] Received %r" % body)
-
-
-channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-print("[*] Waiting for Messages. To Exit press CTRL+C")
-channel.start_consuming()
+if __name__ == '__main__':
+    receiver = Receiver()
+    receiver.start_consuming()
--- Sample/rabbitmq/sender.py
+++ Sample/rabbitmq/sender.py
@@ -1,11 +1,18 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+class Sender(object):
+    """
+    Class which is used for Sending Messages to RabbitMq
+    Default queue='hello', r_key='hello', host='localhost'
+    """
+    def __init__(self, queue='hello') -> None:
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self._channel = self._connection.channel()
+        self._queue = queue
+        self._channel.queue_declare(queue=self._queue)
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print("Message Published")
-connection.close()
-
+    def publish(self, payload, r_key='hello'):
+        self._channel.basic_publish(exchange='', routing_key=str(r_key), body=str(payload))
+        print("Message Publish")
+        self._connection.close()
